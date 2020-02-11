c587f774165bc52af6cda745b57185c86f0bf758
kencharos <many.front@gmail.com>
2020-02-11 17:51:13 +0900

    fix servicea

--- consulservice1/src/main/java/com/github/kmaeda/tutorial/consul/consulservice1/HelloController.java
+++ consulservice1/src/main/java/com/github/kmaeda/tutorial/consul/consulservice1/HelloController.java
@@ -14,6 +14,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Service-A -> " + client.hello();
+        return "Servicea -> " + client.hello();
     }
 }
